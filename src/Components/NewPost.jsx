87d4cfb0a560f86d2dd64929b257b9ea37252753
Leon Forsythe <lvforsythe95@gmail.com>
2023-12-11 14:50:30 -0500

    reconfigured form handler to lift state up

--- src/Components/NewPost.jsx
+++ src/Components/NewPost.jsx
@@ -1,22 +1,13 @@
 import classes from './NewPost.module.css'
 import {useState} from 'react'
 
-function NewPost(){
-
-    const [text, setText] = useState('')
-
-    const handleTextChange = (e) => {
-        console.log(e.target.value)
-        setText(e.target.value);
-    }
-
+function NewPost({bodyHandler}){
     return (
         <form className={classes.form}>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea id="body" required rows={3} onChange={handleTextChange}/>
+                <textarea id="body" required rows={3} onChange={bodyHandler}/>
             </p>
-            <p>{text}</p>
             <p>
                 <label htmlFor="name">Author</label>
                 <input type="text" id="name" required />
--- src/Components/PostsList.jsx
+++ src/Components/PostsList.jsx
@@ -4,12 +4,21 @@ import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
 
 function PostsLists() {
+
+    const [postText, setPostText] = React.useState()
+
+    const textChangeHandler = (e) => {
+        setPostText(e.target.value);
+    }
+
+
   return (
     <>
-      <NewPost />
+      <NewPost bodyHandler={textChangeHandler}/>
       <ul className={classes.postlist}>
         <Post author="Snoopy" body="Merry Christmas" />
         <Post author="Leon" body="I love React" />
+        <Post body={postText}/>
       </ul>
     </>
   );
