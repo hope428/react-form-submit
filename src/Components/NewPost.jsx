7d43237e8c8cac50b6a2a397dba94dc0c05490e4
Leon Forsythe <lvforsythe95@gmail.com>
2023-12-11 14:42:58 -0500

    added change handle function for form

--- src/Components/NewPost.jsx
+++ src/Components/NewPost.jsx
@@ -1,12 +1,22 @@
 import classes from './NewPost.module.css'
+import {useState} from 'react'
 
 function NewPost(){
+
+    const [text, setText] = useState('')
+
+    const handleTextChange = (e) => {
+        console.log(e.target.value)
+        setText(e.target.value);
+    }
+
     return (
         <form className={classes.form}>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea id="body" required rows={3} />
+                <textarea id="body" required rows={3} onChange={handleTextChange}/>
             </p>
+            <p>{text}</p>
             <p>
                 <label htmlFor="name">Author</label>
                 <input type="text" id="name" required />
